6b1acf3c0d658a4817510773251fe3f33de7dd11
Jefferson <jeffersonf23@gmail.com>
2019-05-05 23:19:49 -0300

    Listando todas categorias com DTO

--- src/main/java/com/jeffersonf/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/jeffersonf/cursomc/resources/CategoriaResource.java
@@ -1,9 +1,11 @@
 package com.jeffersonf.cursomc.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.info.ProjectInfoProperties.Build;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.jeffersonf.cursomc.DTO.CategoriaDTO;
 import com.jeffersonf.cursomc.domain.Categoria;
 import com.jeffersonf.cursomc.services.CategoriaService;
 
@@ -51,9 +54,16 @@ public class CategoriaResource {
 	public ResponseEntity<?> delete(@PathVariable Integer id) {		
 		catService.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> delete() {		
+		List<Categoria> list =  catService.findAll();
+		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 		
 	}
-	
+
 	
 
 }
--- src/main/java/com/jeffersonf/cursomc/services/CategoriaService.java
+++ src/main/java/com/jeffersonf/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.jeffersonf.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,8 +40,13 @@ public class CategoriaService {
 		try {
 			catRepo.deleteById(id);
 		}catch(DataIntegrityViolationException e){
-			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos"); 
 		}
 	}
 
+	public List<Categoria> findAll() {
+		return catRepo.findAll();
+	}
+
+	
 }
