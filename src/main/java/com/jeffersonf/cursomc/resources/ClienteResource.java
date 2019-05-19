063745cf12337e0f19c12fc816de1fe4264a0969
Jefferson <jeffersonf23@gmail.com>
2019-05-18 23:42:53 -0300

    Inserindo um novo Cliente com POST

--- src/main/java/com/jeffersonf/cursomc/resources/ClienteResource.java
+++ src/main/java/com/jeffersonf/cursomc/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package com.jeffersonf.cursomc.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,8 +15,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.jeffersonf.cursomc.DTO.ClienteDTO;
+import com.jeffersonf.cursomc.DTO.ClienteNewDTO;
 import com.jeffersonf.cursomc.domain.Cliente;
 import com.jeffersonf.cursomc.services.ClienteService;
 
@@ -36,6 +39,14 @@ public class ClienteResource {
 		
 	 }
 
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDTO){
+		Cliente obj = service.fromDTO(objDTO);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+					.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
 	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDTO, @PathVariable Integer id){
--- src/main/java/com/jeffersonf/cursomc/services/ClienteService.java
+++ src/main/java/com/jeffersonf/cursomc/services/ClienteService.java
@@ -11,8 +11,14 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.jeffersonf.cursomc.DTO.ClienteDTO;
+import com.jeffersonf.cursomc.DTO.ClienteNewDTO;
+import com.jeffersonf.cursomc.domain.Cidade;
 import com.jeffersonf.cursomc.domain.Cliente;
+import com.jeffersonf.cursomc.domain.Endereco;
+import com.jeffersonf.cursomc.domain.enums.TipoCliente;
+import com.jeffersonf.cursomc.repositories.CidadeRepository;
 import com.jeffersonf.cursomc.repositories.ClienteRepository;
+import com.jeffersonf.cursomc.repositories.EnderecoRepository;
 import com.jeffersonf.cursomc.services.exceptions.DataIntegrityException;
 import com.jeffersonf.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -21,6 +27,12 @@ public class ClienteService {
 
 	@Autowired
 	private ClienteRepository repo;
+
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
 	
 	public Cliente find(Integer id) {
 		Optional<Cliente> cliente = repo.findById(id);
@@ -29,6 +41,14 @@ public class ClienteService {
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));		
 		
 		
+	}
+
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		repo.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		
+		return obj;
 	}
 	
 	public Cliente update(Cliente obj) {
@@ -60,6 +80,23 @@ public class ClienteService {
 		return(new Cliente(objDTO.getId(), objDTO.getNome(), objDTO.getEmail(), null, null));	
 	}
 
+	public Cliente fromDTO(ClienteNewDTO objDTO) {
+		Cliente cli = new Cliente(null, objDTO.getNome(), objDTO.getEmail(), objDTO.getCpfOuCnpj(), TipoCliente.toEnum(objDTO.getTipo()));	
+		Cidade cid = new Cidade(objDTO.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDTO.getLogradouro(), objDTO.getNumero(), objDTO.getComplemento(), objDTO.getBairro(), objDTO.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDTO.getTelefone1());
+		
+		if(objDTO.getTelefone2() != null) {
+			cli.getTelefones().add(objDTO.getTelefone2());
+		}
+		
+		if(objDTO.getTelefone3() != null) {
+			cli.getTelefones().add(objDTO.getTelefone3());
+		}
+		return cli;
+	}
+	
 	private void updateDate(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());			
